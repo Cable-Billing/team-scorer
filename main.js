d2abc06096779150b99fdc027f4af15552ff571c
DoneRaging <billing.caleb14@gmail.com>
2018-11-30 08:53:45 +1030

    v

---
+++ main.js
@@ -0,0 +1,19 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app , BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+// Listen for the app to be ready
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({}); // Create new window
+    mainWindow.setMenu(null); // Disable menu bar
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+});

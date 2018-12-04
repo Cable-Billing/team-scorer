0677a082b9f372b32d7453f614955b44ff05aa5a
DoneRaging <Billing.caleb14@gmail.com>
2018-12-04 12:00:29 +1030

    Added style and functionality to team window can now keep track of team's scores

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ app.on('ready', function() {
     }));
 
     // Build menu from template
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    const menu = Menu.buildFromTemplate(menuTemplate);
     // Insert Menu
-    Menu.setApplicationMenu(mainMenu);
+    Menu.setApplicationMenu(menu);
     teamScoreWindow(); // Create team view
     mainWindow.focus(); // Make main window on top
 });
@@ -34,69 +34,23 @@ function teamScoreWindow() {
         protocol: 'file:',
         slashes: true
     }));
-
-    // Build menu from template
-    const teamMenu = Menu.buildFromTemplate(teamMenuTemplate);
-    // Insert Menu
-    Menu.setApplicationMenu(teamMenu);
 }
 
 // Catch team data
 ipcMain.on('team:submit', function(e, teamData) {
     //console.log(teamData);
     teamWindow.webContents.send('team:submit', teamData)
+    teamWindow.focus();
+    teamWindow.setFullScreen(true);
+    teamWindow.setMenuBarVisibility(false);
+    //mainWindow.close();
 });
 
-// Create main menu template
-const mainMenuTemplate = [{
+// Create menu template
+const menuTemplate = [{
     label: 'File',
     submenu: [{
-        label: 'Reload',
-        accelerator: process.platform == 'darwin' ? 'Command+R' : 'Ctrl+R',
-        click() {
-            mainWindow.reload();
-        }
-    }, {
-        label: 'Toggle Developer Tools',
-        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-        click() {
-            mainWindow.toggleDevTools();
-        }
-    }, {
-        label: 'Quit',
-        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-        click() {
-            app.quit();
-        }
-    }]
-}, {
-    label: 'View',
-    submenu: [{
-        label: 'Fullscreen',
-        accelerator: process.platform == 'darwin' ? 'Command+F' : 'Ctrl+F',
-        click() {
-            if (mainWindow.isFullScreen()) {
-                mainWindow.setFullScreen(false);
-                mainWindow.setMenuBarVisibility(true);
-            } else {
-                mainWindow.setFullScreen(true);
-                mainWindow.setMenuBarVisibility(false);
-            }
-        }
-    }]
-}];
-
-// Create team menu template
-const teamMenuTemplate = [{
-    label: 'File',
-    submenu: [{
-        label: 'Reload',
-        accelerator: process.platform == 'darwin' ? 'Command+R' : 'Ctrl+R',
-        click() {
-            teamWindow.reload();
-        }
-    }, {
-        label: 'Toggle Developer Tools',
+        label: 'Dev Tools',
         accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
         click() {
             teamWindow.toggleDevTools();
--- scripts/teamWindow.js
+++ scripts/teamWindow.js
@@ -4,14 +4,25 @@ const { ipcRenderer } = electron;
 ipcRenderer.on('team:submit', function(e, teamData) {
     // Team 1
     document.getElementById('teamOneName').innerHTML = teamData.teamOneName;
-    document.getElementById('teamOne').style.background = teamData.teamOneColour;
+    //document.getElementById('teamOne').style.background = teamData.teamOneColour;
+    document.getElementById('teamOneName').style.color = teamData.teamOneColour;
     // Team 2
     document.getElementById('teamTwoName').innerHTML = teamData.teamTwoName;
-    document.getElementById('teamTwo').style.backgroundColor = teamData.teamTwoColour;
+    //document.getElementById('teamTwo').style.background = teamData.teamTwoColour;
+    document.getElementById('teamTwoName').style.color = teamData.teamTwoColour;
     // Team 3
     document.getElementById('teamThreeName').innerHTML = teamData.teamThreeName;
-    document.getElementById('teamThree').style.backgroundColor = teamData.teamThreeColour;
+    //document.getElementById('teamThree').style.background = teamData.teamThreeColour;
+    document.getElementById('teamThreeName').style.color = teamData.teamThreeColour;
     // Team 4
     document.getElementById('teamFourName').innerHTML = teamData.teamFourName;
-    document.getElementById('teamFour').style.backgroundColor = teamData.teamFourColour;
+    //document.getElementById('teamFour').style.background = teamData.teamFourColour;
+    document.getElementById('teamFourName').style.color = teamData.teamFourColour;
 });
+
+function addScoreToTeam(teamScoreElement, amount) {
+    var scoreElement = document.getElementById(teamScoreElement);
+    var currentScore = scoreElement.innerHTML;
+    var newScore = parseInt(currentScore) + parseInt(amount);
+    scoreElement.innerHTML = newScore;
+}

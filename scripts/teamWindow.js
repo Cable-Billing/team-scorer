3933fbbdf410283151b8ea80b452b8db6f1b18fd
DoneRaging <billing.caleb14@gmail.com>
2018-12-23 18:43:27 +1030

    Made team name text to resize when too long added colours: pink removed colours: sky blue, dark green changed colours: light blue -> blue, light green -> green altered coluors: orange, red

--- scripts/teamWindow.js
+++ scripts/teamWindow.js
@@ -2,20 +2,47 @@ const electron = require('electron');
 const { ipcRenderer } = electron;
 
 ipcRenderer.on('team-submit', function(e, teamData) {
+
+    // Check length of team name and resize accordingly
     // Team 1
-    document.getElementById('teamOneName').innerHTML = teamData.teamOneName;
+    if (teamData.teamOneName.length > 15) {
+        document.getElementById('teamOneName').style.fontSize = "6vh";
+    } else {
+        document.getElementById('teamOneName').style.fontSize = "8vh";
+    }
+    // Team 2
+    if (teamData.teamTwoName.length > 15) {
+        document.getElementById('teamTwoName').style.fontSize = "6vh";
+    } else {
+        document.getElementById('teamTwoName').style.fontSize = "8vh";
+    }
+    // Team 3
+    if (teamData.teamThreeName.length > 15) {
+        document.getElementById('teamThreeName').style.fontSize = "6vh";
+    } else {
+        document.getElementById('teamThreeName').style.fontSize = "8vh";
+    }
+    // Team 4
+    if (teamData.teamFourName.length > 15) {
+        document.getElementById('teamFourName').style.fontSize = "6vh";
+    } else {
+        document.getElementById('teamFourName').style.fontSize = "8vh";
+    }
+
+    // Team 1
+    document.getElementById('teamOneName').innerHTML = teamData.teamOneName.toUpperCase();
     document.getElementById('teamOneScore').style.background = teamData.teamOneColour;
     document.getElementById('teamOneName').style.color = teamData.teamOneColour;
     // Team 2
-    document.getElementById('teamTwoName').innerHTML = teamData.teamTwoName;
+    document.getElementById('teamTwoName').innerHTML = teamData.teamTwoName.toUpperCase();
     document.getElementById('teamTwoScore').style.background = teamData.teamTwoColour;
     document.getElementById('teamTwoName').style.color = teamData.teamTwoColour;
     // Team 3
-    document.getElementById('teamThreeName').innerHTML = teamData.teamThreeName;
+    document.getElementById('teamThreeName').innerHTML = teamData.teamThreeName.toUpperCase();
     document.getElementById('teamThreeScore').style.background = teamData.teamThreeColour;
     document.getElementById('teamThreeName').style.color = teamData.teamThreeColour;
     // Team 4
-    document.getElementById('teamFourName').innerHTML = teamData.teamFourName;
+    document.getElementById('teamFourName').innerHTML = teamData.teamFourName.toUpperCase();
     document.getElementById('teamFourScore').style.background = teamData.teamFourColour;
     document.getElementById('teamFourName').style.color = teamData.teamFourColour;
 });
